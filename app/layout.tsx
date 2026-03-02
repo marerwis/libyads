e5879cb7e45991ed5fde0e68263122efc5ec0259
marerwis <marerwis@gmail.com>
2026-03-02 04:53:06 +0300

    chore: add Facebook SDK script to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -47,6 +48,29 @@ export default function RootLayout({
             <SpeedInsights />
           </LanguageProvider>
         </ThemeProvider>
+
+        <Script id="facebook-jssdk" strategy="lazyOnload">
+          {`
+            window.fbAsyncInit = function() {
+              FB.init({
+                appId      : '146492924904048',
+                cookie     : true,
+                xfbml      : true,
+                version    : 'v21.0'
+              });
+                
+              FB.AppEvents.logPageView();   
+            };
+          
+            (function(d, s, id){
+               var js, fjs = d.getElementsByTagName(s)[0];
+               if (d.getElementById(id)) {return;}
+               js = d.createElement(s); js.id = id;
+               js.src = "https://connect.facebook.net/en_US/sdk.js";
+               fjs.parentNode.insertBefore(js, fjs);
+             }(document, 'script', 'facebook-jssdk'));
+          `}
+        </Script>
       </body>
     </html>
   );
